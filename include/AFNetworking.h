f8f7d32bdb0488526a967779d55e206bfc089cfa
danielebogo <me@bogodaniele.com>
2026-01-29 23:48:20 +0000

    Include all public headers in SwiftPM umbrella

--- include/AFNetworking.h
+++ include/AFNetworking.h
@@ -1,2 +1,35 @@
-#import "../AFNetworking/AFNetworking.h"
-#import "../UIKit+AFNetworking/UIKit+AFNetworking.h"
+// AFNetworking.h (SwiftPM umbrella)
+//
+// This umbrella header is used by SwiftPM. It must include all public headers
+// under `include/` to avoid incomplete-umbrella warnings.
+
+#import <Foundation/Foundation.h>
+#import <Availability.h>
+#import <TargetConditionals.h>
+
+#ifndef _AFNETWORKING_
+    #define _AFNETWORKING_
+
+    #import "AFURLRequestSerialization.h"
+    #import "AFURLResponseSerialization.h"
+    #import "AFSecurityPolicy.h"
+
+#if !TARGET_OS_WATCH
+    #import "AFNetworkReachabilityManager.h"
+#endif
+
+    #import "AFURLSessionManager.h"
+    #import "AFHTTPSessionManager.h"
+
+    // UIKit+AFNetworking (headers are internally platform-guarded)
+    #import "AFAutoPurgingImageCache.h"
+    #import "AFImageDownloader.h"
+    #import "UIActivityIndicatorView+AFNetworking.h"
+    #import "UIButton+AFNetworking.h"
+    #import "UIImageView+AFNetworking.h"
+    #import "UIProgressView+AFNetworking.h"
+    #import "AFNetworkActivityIndicatorManager.h"
+    #import "UIRefreshControl+AFNetworking.h"
+    #import "WKWebView+AFNetworking.h"
+
+#endif /* _AFNETWORKING_ */
